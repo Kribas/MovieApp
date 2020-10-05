bf1cdbc55c4a620393848b75bc13d4af541a198a
Kribas Rimal <carlousrimal@gmail.com>
2020-10-05 10:35:51 +0545

    made changes

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -1,4 +1,6 @@
 import 'package:dio/dio.dart';
+import 'package:movieapp/model/cast_response.dart';
+import 'package:movieapp/model/movie_detail_response.dart';
 import 'package:movieapp/model/movie_response.dart';
 import 'package:movieapp/model/genre_response.dart';
 import 'package:movieapp/model/person_response.dart';
@@ -12,6 +14,7 @@ class MovieRepository {
   var getPlayingUrl = '$mainUrl/movie/now_playing';
   var getGenresUrl = '$mainUrl/genre/movie/list';
   var getPersonsUrl = '$mainUrl/person/popular';
+  var movieUrl = '$mainUrl/movie';
 
   Future<MovieResponse> getMovies() async {
     var params = {
@@ -95,5 +98,54 @@ class MovieRepository {
 
     }
   }
+
+  Future<MovieDetailResponse> getMovieDetail(int id) async {
+    var params = {
+      "api_key": apiKey,
+      "language": "en-US"
+    };
+    try {
+      Response response = await _dio.get(movieUrl + "/$id", queryParameters: params);
+      return MovieDetailResponse.fromJson(response.data);
+      
+    }catch(error, stacktrace) {
+      print('Exception occured: $error stackTrace: $stacktrace');
+      return MovieDetailResponse.withError("$error");
+    }
+  }
+
+  Future<CastResponse> getCasts(int id) async {
+    var params = {
+      "api_key": apiKey,
+      "language": "en-US"
+    };
+    
+    try {
+      Response response = await _dio.get(movieUrl + "/$id" + "/credits", queryParameters: params );
+      return CastResponse.fromJson(response.data);
+    }catch(error,stacktrace) {
+      print('Exception occured: $error stackTrace: $stacktrace');
+      return CastResponse.withError("$error");
+
+    }
+  }
+
+  Future<MovieResponse> getSimilarMovies(int id) async {
+    var params = {
+      "api_key": apiKey,
+      "language": "en-US"
+    };
+
+    try {
+      Response response = await _dio.get(movieUrl + "/$id" + "/similar", queryParameters: params );
+      return MovieResponse.fromJson(response.data);
+    }catch(error,stacktrace) {
+      print('Exception occured: $error stackTrace: $stacktrace');
+      return MovieResponse.withError("$error");
+
+    }
+  }
+
+
 
 }
